ebda16c82c9805d1af822f08348140e2901e9c4f
Matin <matinhosseiniofficial@gmail.com>
2024-07-21 10:58:38 +0330

    added color prop for ThreeDotsLoading

--- src/components/ThreeDotLoading/index.jsx
+++ src/components/ThreeDotLoading/index.jsx
@@ -1,7 +1,16 @@
 import "./index.css";
 
-const ThreeDotsLoading = () => {
-  return <div className={"loader"}></div>;
+const ThreeDotsLoading = ({ color }) => {
+  return (
+    <div
+      className="loader"
+      style={{
+        "--_g": `no-repeat radial-gradient(circle closest-side, ${
+          color && "#000"
+        } 90%, #0000)`,
+      }}
+    ></div>
+  );
 };
 
 export default ThreeDotsLoading;
