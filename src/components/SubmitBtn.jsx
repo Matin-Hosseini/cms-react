c29f0683a068d7f5ad8acede0ed48a3dc91f767c
Matin <matinhosseiniofficial@gmail.com>
2024-07-21 11:21:50 +0330

    SubmitBtn component created

---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@mui/material";
+import ThreeDotsLoading from "./ThreeDotLoading";
+
+/**
+ * @param {import('@mui/material/Button').ButtonProps} props
+ */
+
+const SubmitBtn = ({ children, isSubmitting, ...props }) => {
+  return (
+    <Button
+      {...props}
+      type="submit"
+      disabled={isSubmitting}
+      variant={props.variant || "contained"}
+      className={`h-12 flex items-center justify-center ${
+        props.backGround || "bg-gray-500"
+      } ${props.className}`}
+      fullWidth={props.fullWidth || true}
+    >
+      {isSubmitting ? <ThreeDotsLoading color={"#fff"} /> : <>{children}</>}
+    </Button>
+  );
+};
+
+export default SubmitBtn;
