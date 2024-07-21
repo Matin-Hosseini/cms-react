013604e6ab1bbd0fdde8979527d649f1055458be
Matin <matinhosseiniofficial@gmail.com>
2024-07-21 12:28:26 +0330

    sms schema

---
+++ src/validations/schemas/panelSms.js
@@ -0,0 +1,15 @@
+import { z } from "zod";
+import { phoneRegex } from "../../utils/regexs";
+
+export const addSMSCategorySchema = z.object({
+  title: z.string().min(1, "لطفا عنوان را وارد کنید."),
+  text: z.string().min(1, "لطفا متن پیام را وارد کنید."),
+  description: z.string().min(1, "لطفا توضیحات را وارد کنید."),
+});
+
+export const sendSMSSchema = z.object({
+  phoneNumber: z
+    .string()
+    .min(1, "لطفا شماره موبایل را وارد کنید.")
+    .regex(phoneRegex, "شماره موبایل معتبر نمی باشد."),
+});
