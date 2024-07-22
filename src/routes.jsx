d503072b65ed4680a0d5d16af56a5cd1a13941cc
Matin <matinhosseiniofficial@gmail.com>
2024-07-22 14:38:55 +0330

    new router

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,12 +4,14 @@ const Home = lazy(() => import("./pages/home"));
 const Login = lazy(() => import("./pages/login"));
 const AddSMSCategory = lazy(() => import("./pages/AddSMSCategory"));
 const SendSms = lazy(() => import("./pages/sendSms"));
+const AllSentSMSs = lazy(() => import("./pages/AllSentSMSs"));
 
 const routes = [
   { path: "/", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/add-sms-category", element: <AddSMSCategory /> },
   { path: "/send-sms", element: <SendSms /> },
+  { path: "/all-sent-messages", element: <AllSentSMSs /> },
 ];
 
 export default routes;
