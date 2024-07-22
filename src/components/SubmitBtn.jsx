8cd27d01c8801d7d0370e73d9d1cf4e7040b706c
Matin <matinhosseiniofficial@gmail.com>
2024-07-22 14:39:30 +0330

    type submit

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -9,11 +9,12 @@ const SubmitBtn = ({ children, isSubmitting, ...props }) => {
   return (
     <Button
       {...props}
-      type="submit"
+      type={props.type || "submit"}
       disabled={isSubmitting || props.disabled}
       variant={props.variant || "contained"}
       className={`h-12 flex items-center justify-center ${
-        props.backGround || "bg-gray-500 disabled:text-gray-200 disabled:opacity-70"
+        props.backGround ||
+        "bg-gray-500 disabled:text-gray-200 disabled:opacity-70"
       } ${props.className}`}
       fullWidth={props.fullWidth || true}
     >
