eaeca5b13b34c34c1cfd7cdc467696d30ae33e95
Matin <matinhosseiniofficial@gmail.com>
2024-07-25 09:31:02 +0330

    new routes

---
+++ data/pages.jsx
@@ -0,0 +1,53 @@
+import { PiUsersThreeLight } from "react-icons/pi";
+import { GiTrophyCup } from "react-icons/gi";
+import { TbMessage2 } from "react-icons/tb";
+import { FiUsers } from "react-icons/fi";
+import { MdOutlineSwitchAccessShortcut } from "react-icons/md";
+import { AiOutlineCluster } from "react-icons/ai";
+
+const pages = [
+  {
+    id: 1,
+    route: "/customers",
+    title: "مشتریان",
+    icon: <PiUsersThreeLight />,
+    permissionIDs: [9, 10, 15, 25],
+  },
+  {
+    id: 2,
+    route: "/cup",
+    title: "کاپ",
+    icon: <GiTrophyCup />,
+    permissionIDs: [12, 13, 14],
+  },
+  {
+    id: 3,
+    route: "/sms",
+    title: "پیامک ها",
+    icon: <TbMessage2 />,
+    permissionIDs: [22, 23, 24, 27, 28, 29, 30],
+  },
+  {
+    id: 4,
+    route: "/users",
+    title: "کاربران",
+    icon: <FiUsers />,
+    permissionIDs: [16, 18, 19, 26],
+  },
+  {
+    id: 5,
+    route: "/permissions",
+    title: "دسترسی ها",
+    icon: <MdOutlineSwitchAccessShortcut />,
+    permissionIDs: [1, 2, 3, 16, 17],
+  },
+  {
+    id: 6,
+    route: "/roles",
+    title: "نقش ها",
+    icon: <AiOutlineCluster />,
+    permissionIDs: [4, 5, 6, 17],
+  },
+];
+
+export default pages;
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,8 +4,11 @@ const Home = lazy(() => import("./pages/home"));
 const Login = lazy(() => import("./pages/login"));
 const AddSMSCategory = lazy(() => import("./pages/AddSMSCategory"));
 const SendSms = lazy(() => import("./pages/sendSms"));
-const AllSentSMSs = lazy(() => import("./pages/AllSentSMSs"));
+// const AllSentSMSs = lazy(() => import("./pages/AllSentSMSs/index/"));
 const ProtectedRoute = lazy(() => import("./utils/ProtectedRoute"));
+const IsProgressing = lazy(() => import("./pages/IsProgressing"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Sms = lazy(() => import("./pages/Sms"));
 
 const routes = [
   { path: "/login", element: <Login /> },
@@ -14,9 +17,16 @@ const routes = [
     element: <ProtectedRoute />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/add-sms-category", element: <AddSMSCategory /> },
+      { path: "/customers", element: <Home /> },
+      { path: "/cup", element: <IsProgressing /> },
+      { path: "/sms", element: <Sms /> },
+      { path: "/users", element: <IsProgressing /> },
+      { path: "/permissions", element: <IsProgressing /> },
+      { path: "/permissions", element: <IsProgressing /> },
+      { path: "/roles", element: <IsProgressing /> },
       { path: "/send-sms", element: <SendSms /> },
-      { path: "/all-sent-messages", element: <AllSentSMSs /> },
+      // { path: "/all-sent-messages", element: <AllSentSMSs /> },
+      { path: "/*", element: <NotFound /> },
     ],
   },
 ];
