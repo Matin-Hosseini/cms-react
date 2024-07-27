745b74545638725d17bc162615d7da13dfdad015
Matin <matinhosseiniofficial@gmail.com>
2024-07-27 13:15:14 +0330

    implementing error layout

--- src/pages/IsProgressing/index.jsx
+++ src/pages/IsProgressing/index.jsx
@@ -1,19 +1,12 @@
+import ErrorLayout from "../../components/ErrorLayout";
 import codingGif from "./../../assets/gifs/coding.gif";
 
 const IsProgressing = () => {
   return (
-    <div className="h-[calc(100dvh-124px)] flex flex-col items-center justify-center gap-5">
-      <img
-        src={codingGif}
-        alt="در حال توسعه"
-        className="max-w-[500px] w-[90%]"
-      />
-      <div>
-        <h2 className="font-[Persian-titraj] text-3xl md:text-4xl text-[#6E6F71]  text-center">
-          این صفحه در دست توسعه می باشد. از صبر و شکیبایی شما سپاسگذاریم.
-        </h2>
-      </div>
-    </div>
+    <ErrorLayout
+      img={codingGif}
+      title={"این صفحه در حال توسه می باشد. از صبر و شکیبایی شما سپاسگذاریم."}
+    />
   );
 };
 
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,18 +1,8 @@
+import ErrorLayout from "../../components/ErrorLayout";
 import notFoundImg from "./../../assets/images/not-found.jpg";
 
 const NotFound = () => {
-  return (
-    <div className="h-[calc(100dvh-124px)] flex flex-col gap-5 items-center justify-center">
-      <img
-        src={notFoundImg}
-        alt="صفحه مورد نظر یافت نشد."
-        className="max-w-[550px]"
-      />
-      <h2 className="font-[Persian-titraj] text-5xl text-blue-300">
-        صفحه مورد نظر شما یافت نشد
-      </h2>
-    </div>
-  );
+  return <ErrorLayout img={notFoundImg} title={"صفحه مورد نظر یافت نشد."} />;
 };
 
 export default NotFound;
