4941c9ab38e1c27dabc0c5ae9e83be80ba17d96f
Matin <matinhosseiniofficial@gmail.com>
2024-07-27 13:13:24 +0330

    handling access for each component

--- src/HOCs/withPermission.jsx
+++ src/HOCs/withPermission.jsx
@@ -1,12 +1,25 @@
-const WithPermission = (Component, userPermissions, componentPermissionId) => {
-  const permissionIDs = userPermissions.map(
+import { ConstructionRounded } from "@mui/icons-material";
+import { useAuthContext } from "../contexts/auth";
+
+const WithPermission = (Component, componentPermissionId, ErrorComponent) => {
+  const {
+    userInfo: { permissions },
+  } = useAuthContext();
+
+  const permissionIDs = permissions.map(
     (permission) => permission.permission_Id
   );
 
   const hasAccess = permissionIDs.some((id) => id === componentPermissionId);
 
   return (props) => {
-    return hasAccess ? <Component {...props} /> : <></>;
+    return hasAccess ? (
+      <Component {...props} />
+    ) : ErrorComponent ? (
+      <ErrorComponent />
+    ) : (
+      <></>
+    );
   };
 };
 
