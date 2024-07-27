2d05bcf4fb63cfb58f96834729ba362ad6a92017
Matin <matinhosseiniofficial@gmail.com>
2024-07-27 10:33:43 +0330

    routes update

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,7 @@ import { lazy } from "react";
 
 const Home = lazy(() => import("./pages/home"));
 const Login = lazy(() => import("./pages/login"));
-const AddSMSCategory = lazy(() => import("./pages/AddSMSCategory"));
-const SendSms = lazy(() => import("./pages/sendSms"));
-// const AllSentSMSs = lazy(() => import("./pages/AllSentSMSs/index/"));
+
 const ProtectedRoute = lazy(() => import("./utils/ProtectedRoute"));
 const IsProgressing = lazy(() => import("./pages/IsProgressing"));
 const NotFound = lazy(() => import("./pages/NotFound"));
@@ -24,8 +22,6 @@ const routes = [
       { path: "/permissions", element: <IsProgressing /> },
       { path: "/permissions", element: <IsProgressing /> },
       { path: "/roles", element: <IsProgressing /> },
-      { path: "/send-sms", element: <SendSms /> },
-      // { path: "/all-sent-messages", element: <AllSentSMSs /> },
       { path: "/*", element: <NotFound /> },
     ],
   },
