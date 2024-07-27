6b23613e53e907ee0b59e789beef51b83cb76a97
Matin <matinhosseiniofficial@gmail.com>
2024-07-27 10:34:06 +0330

    all sms categories data for table

---
+++ data/tables/allSmsCategories.js
@@ -0,0 +1,10 @@
+export const columns = [
+  { field: "title", headerName: "عنوان", width: 80, editable: true },
+  { field: "text", headerName: "متن پیام", width: 500, editable: true },
+  {
+    field: "description",
+    headerName: "توضیحات",
+    width: 300,
+    editable: true,
+  },
+];
