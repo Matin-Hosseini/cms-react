60e1a87e335ac5305ac3e0104919f29d82b77dc5
Matin <matinhosseiniofficial@gmail.com>
2024-07-29 17:15:29 +0330

    react query

--- src/main.jsx
+++ src/main.jsx
@@ -8,17 +8,24 @@ import AuthProvider from "./contexts/auth";
 import SnackbarProvider from "./contexts/snackbar.jsx";
 import MuiProvider from "./contexts/mui.jsx";
 import UserProvider from "./contexts/user.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
-      <UserProvider>
-        <MuiProvider>
-          <SnackbarProvider>
-            <App />
-          </SnackbarProvider>
-        </MuiProvider>
-      </UserProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <UserProvider>
+          <MuiProvider>
+            <SnackbarProvider>
+              <App />
+            </SnackbarProvider>
+          </MuiProvider>
+        </UserProvider>
+      </AuthProvider>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   </BrowserRouter>
 );
