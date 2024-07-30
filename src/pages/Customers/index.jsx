596b49fc927ac33ee29e95e1a52810cc12fecb87
Matin <matinhosseiniofficial@gmail.com>
2024-07-30 13:45:20 +0330

    customers with react query

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -1,9 +1,26 @@
-const Customers = () => {
-  return (
-    <div>
-      <h2>صفحه مشتریان</h2>
-    </div>
-  );
-};
+import AddNewCustomer from "../../components/AddNewCustomer";
+import Table from "../../components/Table";
+import Cookies from "js-cookie";
+import { useQuery } from "@tanstack/react-query";
+import { getAllCustomers } from "../../services/requests/customers";
 
-export default Customers;
+export default function Home() {
+  const { data } = useQuery({
+    queryKey: ["customers"],
+    queryFn: async () =>
+      await getAllCustomers({
+        typeOfRegisterCustomer_Id: null,
+        registerDate: null,
+        phoneNumber: null,
+        nationalCode: null,
+        token: Cookies.get("token"),
+      }),
+  });
+
+  return (
+    <>
+      <AddNewCustomer />
+      <Table customers={data?.result.informationCustomer || []} />
+    </>
+  );
+}
