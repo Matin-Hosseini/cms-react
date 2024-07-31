58440d745afb618bc1cbc72d294de48d5c3c87ea
Matin <matinhosseiniofficial@gmail.com>
2024-07-31 16:10:24 +0330

    add user role implementation

--- src/pages/Users/components/AddUser.jsx
+++ src/pages/Users/components/AddUser.jsx
@@ -1,20 +1,32 @@
-import { Alert, Box, Dialog, DialogContent, TextField } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Dialog,
+  DialogContent,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import CategoryBtnBox from "../../../components/CategoryBtnBox";
 import addUserIcon from "./../../../assets/images/icons/add-user.png";
 import DialogHeader from "../../../components/DialogHeader";
 import { useState } from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@emotion/react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import SubmitBtn from "../../../components/SubmitBtn";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSnackbar } from "../../../contexts/snackbar";
 import Cookies from "js-cookie";
 import { addUserSchema } from "../../../validations/schemas/user";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { addUser } from "../../../services/requests/users";
+import { getAllRoles } from "../../../services/requests/role";
 
-const AddUser = () => {
+const AddUser = ({ roles }) => {
   const [open, setOpen] = useState(false);
 
   const theme = useTheme();
@@ -25,10 +37,23 @@ const AddUser = () => {
 
   const token = Cookies.get("token");
 
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(addUserSchema) });
+
+  const roleQuery = useQuery({
+    queryKey: ["roles"],
+    queryFn: async () => await getAllRoles(token),
+  });
+
   const mutation = useMutation({
     mutationFn: async (data) => await addUser(data),
     onSuccess: (data) => {
-      console.log(data);
+      reset();
       showSnackbar("کاربر جدید افزوده شد.");
 
       queryClient.invalidateQueries(["users"]);
@@ -38,17 +63,12 @@ const AddUser = () => {
     },
   });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: zodResolver(addUserSchema) });
-
-  const submitHandler = async (data) => {
+  const submitHandler = async ({ userName, password, role_ID }) => {
     mutation.mutate({
       token,
-      ...data,
-      role_ID: 0,
+      userName,
+      password,
+      role_ID: +role_ID,
     });
   };
 
@@ -85,6 +105,26 @@ const AddUser = () => {
               className=""
               onSubmit={handleSubmit(submitHandler)}
             >
+              <FormControl className="mb-3" error={!!errors.role_ID} fullWidth>
+                <InputLabel id="role-select">نقش</InputLabel>
+                <Controller
+                  name="role_ID"
+                  control={control}
+                  defaultValue={""}
+                  render={({ field }) => (
+                    <Select labelId="role-select" label="Role" {...field}>
+                      {roleQuery.data?.result.roles.map((role) => (
+                        <MenuItem key={role.id} value={role.id.toString()}>
+                          {role.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  )}
+                />
+                <FormHelperText>
+                  {!!errors.role_ID && errors.role_ID.message}
+                </FormHelperText>
+              </FormControl>
               <TextField
                 fullWidth
                 className="mb-3"
