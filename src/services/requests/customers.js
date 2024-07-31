0198e46738f3b3c9437c3bffd56bbd74d5ecd9d7
Matin <matinhosseiniofficial@gmail.com>
2024-07-31 16:10:36 +0330

    update customers query

--- src/services/requests/customers.js
+++ src/services/requests/customers.js
@@ -2,3 +2,6 @@ import Api from "../api";
 
 export const getAllCustomers = async (data) =>
   (await Api.post("/Customer/GetAllCustomers", data)).data;
+
+export const getCustomerGameDetails = async (data) =>
+  (await Api.post("/Gaming/ShowDetailCustmersGaming", data)).data;
