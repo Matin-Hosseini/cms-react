f472a43f1765ee7b31fa92e4161cab385747e765
Matin <matinhosseiniofficial@gmail.com>
2024-08-04 15:58:16 +0330

    changed roles to is progressing

--- src/routes.jsx
+++ src/routes.jsx
@@ -70,7 +70,7 @@ const routes = [
         path: "/roles",
         element: (
           <DoesNotHaveAccess>
-            <Role />
+            <IsProgressing />
           </DoesNotHaveAccess>
         ),
       },
