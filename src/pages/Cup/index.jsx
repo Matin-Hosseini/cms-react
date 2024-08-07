b2b4ed9a83b30aefd9be7272ddf7d4e44c99a6ff
Matin <matinhosseiniofficial@gmail.com>
2024-08-07 16:39:50 +0330

    cup page

--- src/pages/Cup/index.jsx
+++ src/pages/Cup/index.jsx
@@ -1,6 +1,15 @@
+import AddCup from "./Components/AddCup";
+import AddCupItem from "./Components/AddCupItem";
+import AllCups from "./Components/AllCups";
+
 const Cup = () => {
   return (
     <div>
+      <div className="grid sm:grid-cols-2 gap-3 mb-6">
+        <AddCup />
+        <AddCupItem />
+      </div>
+      <AllCups />
       <h2>صفحه کاپ</h2>
     </div>
   );
