0dac91dadd02925f88d6eefa5649ee6fc8e77d0c
Matin <matinhosseiniofficial@gmail.com>
2024-08-14 14:58:56 +0330

    gaming request update

--- src/services/requests/gaming.js
+++ src/services/requests/gaming.js
@@ -2,3 +2,6 @@ import Api from "../api";
 
 export const addCustomerGaming = async (data) =>
   (await Api.post("/Gaming/AddCustmerGaming", data)).data;
+
+export const setWinner = async (data) =>
+  (await Api.post("/Gaming/UpdateWinnerCustmersGaming", data)).data;
